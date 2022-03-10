1633738aeac6086a3d01b6fe90ddc58590874b01
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-10 14:15:02 -0500

    better path handling

--- display-ip.sh
+++ display-ip.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Path: 
+cd /home/justin/build/get-IP-address
 
 g++ -o getIPaddress getIPaddress.cpp
+
+touch /home/justin/ip-info
+cd /home/justin/
 
 ip addr | grep inet > ip-info
 
 # call c++ program here:
 
-./getIPaddress
+./build/get-IP-address/getIPaddress
 
 # program will grab the first line only and parse string for just the ip
 # will then output to screen in program
-
 
