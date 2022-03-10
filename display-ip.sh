cac96f57b6abe20c6c7ccc0fd03163eb562d4533
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-10 03:44:17 -0600

    updated paths

--- display-ip.sh
+++ display-ip.sh
@@ -4,7 +4,7 @@ ip addr | grep inet > ip-info
 
 # call c++ program here:
 
-./build/ip-display/getIPaddress
+./build/get-IP-address/getIPaddress
 
 # program will grab the first line only and parse string for just the ip
 # will then output to screen in program
