56c3ab420672d4ef6f54771d639ad227251c619c
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-10 14:33:08 -0600

    Update display-ip.sh

--- display-ip.sh
+++ display-ip.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-cd /home/justin/build/get-IP-address
+cd /home/$USER/build/get-IP-address
 
 g++ -o getIPaddress getIPaddress.cpp
 
-touch /home/justin/ip-info
-cd /home/justin/
+touch /home/$USER/ip-info
+cd /home/$USER/
 
 ip addr | grep inet > ip-info
 
+# store user home path: 
+
+userPath=`echo /home/$USER`
+
 # call c++ program here:
 
-./build/get-IP-address/getIPaddress
+./build/get-IP-address/getIPaddress $userPath 
 
 # program will grab the first line only and parse string for just the ip
 # will then output to screen in program
+
 
